366249d27b43de138c3505827fa7e0bb7921e3b0
William L. Thomson Jr <wlt@o-sinc.com>
2023-01-04 12:28:38 -0500

    static/controls.js: Revised & simplified commnication with server backend
    - submitForm() function handles all communication between UI and server
    - Other functions simply control the web UI and do not interact with server

--- static/controls.js
+++ static/controls.js
@@ -1,52 +1,18 @@
-/*
-    var actions = ['move_backward', 'move_forward', 'stop', 'turn_left', 'turn_right'];
-    function buttonActions(acts) {
-        for(var i = 0; i < acts.length; i++) {
-                $('a#'+acts[i]).on('click', function(e) {
-                   console.log("action "+acts[i]);
-                  e.preventDefault()
-                  $.getJSON('/test'+acts[i],
-                      function(data) {
-                      //do nothing
-                  });
-                  return false;
-                });
-            }
-        }
-        $(function() {
-            buttonActions(actions);
-        });
+/* Copyright 2020-2023 Florida State College at Jacksonville
+* Author William L. Thomson Jr. <w@wltjr.com>
+*
+* Distributed under the terms of The GNU Public License v3.0 (GPLv3)
 */
-$(function () {
-    $('a#move_backward').on('click', function (e) {
-        e.preventDefault()
-        $.getJSON('/move_backward',
-            function (data) {
-                //do nothing
-            });
-        return false;
+
+function submitForm(e) {
+    $("#action").val(window.event.target.innerHTML);
+    $.ajax({
+        url: "/action",
+        type: 'post',
+        dataType: 'json',
+        data: $("#controls").serialize()
     });
-});
-$(function () {
-    $('a#move_forward').on('click', function (e) {
-        e.preventDefault()
-        $.getJSON('/move_forward',
-            function (data) {
-                //do nothing
-            });
-        return false;
-    });
-});
-$(function () {
-    $('a#stop').on('click', function (e) {
-        e.preventDefault()
-        $.getJSON('/stop',
-            function (data) {
-                //do nothing
-            });
-        return false;
-    });
-});
+}
 $(function () {
     $("#speed_lock").click(function () {
         if ($(this).is(":checked")) {
