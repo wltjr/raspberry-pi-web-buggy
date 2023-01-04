b333b32966f08a10de3794c6420c5ee27d045f1c
William L. Thomson Jr <wlt@o-sinc.com>
2023-01-04 12:36:13 -0500

    web-buggy.py: Functional, revised, simplified and less redudant code backend
    - Provides intial basic function and control of robot, general speed,
      forward/backward movement, and ability to stop

--- web-buggy.py
+++ web-buggy.py
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Copyright 2020-2023 Florida State College at Jacksonville
-# Author William L. Thomson Jr.
-#        w@wltjr.com
+# Author William L. Thomson Jr. <w@wltjr.com>
 #
 # Distributed under the terms of The GNU Public License v3.0 (GPLv3)
 
 #Import flask
-from flask import Flask, render_template, Response, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 #Import time
@@ -26,34 +25,29 @@ robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 def index():
 	return render_template('index.html');
 
-@app.route('/move_backward')
-def move_backward():
-	robot.backward(100)
-	return ("nothing")
+# handle form submissions
+@app.route('/action', methods=['POST'])
+def submit():
+	# get form data in json format
+	data = jsonify(request.form).json
 
-@app.route('/move_forward')
-def move_forward():
-	robot.forward(100)
-	return ("nothing")
+	# set variables from form elements
+	action = data['action']
+	speed_left = int(data['speed_left'])
+	speed_right = int(data['speed_right'])
 
-@app.route('/stop')
-def stop():
-	robot.stop()
-	return ("nothing")
+	# default to slowest speed
+	speed = speed_left if speed_left < speed_right else speed_right
 
-@app.route('/turn_left')
-def turn_left():
-	robot.left(100, 3)
-	return ("nothing")
+	# switch action form value
+	if action == "Backward":
+		robot.backward(speed)
+	elif action == "Forward":
+		robot.forward(speed)
+	elif action == "Stop":
+		robot.stop()
 
-@app.route('/turn_right')
-def turn_right():
-	robot.right(100, 3)
 	return ("nothing")
-
-@app.route('/json')
-def json():
-    return render_template('json.html')
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port=5000)
